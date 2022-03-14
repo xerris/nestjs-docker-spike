d9c8db09342c3aa4c2b553833c1bb6cf634d567e
Greg Cook <greg@xerris.com>
2022-03-14 07:21:49 -0600

    added the lamda.ts file which you use as the proxy file to the AWS Lambda

---
+++ src/lambda.ts
@@ -0,0 +1,40 @@
+import { Server } from 'http';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import * as serverlessExpress from 'aws-serverless-express';
+import * as express from 'express';
+import { Express } from 'express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+let lambdaProxy: Server;
+
+async function bootstrap() {
+  const expressServer: Express = express();
+  const nestApp = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressServer),
+  );
+  await nestApp.init();
+  return serverlessExpress.createServer(expressServer, null, []);
+}
+
+bootstrap().then((server) => (lambdaProxy = server));
+
+function waitForServer(event: any, context: any) {
+  setImmediate(() => {
+    if (!lambdaProxy) {
+      waitForServer(event, context);
+    } else {
+      serverlessExpress.proxy(lambdaProxy, event, context);
+    }
+  });
+}
+
+export const handler = (event: APIGatewayEvent, context: Context) => {
+  if (lambdaProxy) {
+    serverlessExpress.proxy(lambdaProxy, event, context as any);
+  } else {
+    waitForServer(event, context);
+  }
+};
