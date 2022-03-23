a24087d89f9c737a1d73da05e7862a36cf6bd667
Greg Cook <greg@xerris.com>
2022-03-22 19:12:30 -0600

    infrastructure folder with cdk example included

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path';
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new NodejsFunction(this, 'my-function', {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'main',
+      entry: path.join(__dirname, `/../../src/sample-lambda.ts`),
+    });
+  }
+}
